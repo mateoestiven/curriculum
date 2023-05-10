1e0b35224087161883495feb59ec9d8c0c34a91d
mateoestiven <pallaresjacomem@gmail.com>
2023-05-10 17:06:55 -0500

    add javascript in the index

---
+++ js/clase.js
@@ -0,0 +1,36 @@
+//variables
+const nameuser = "MateoEstiven";
+var x = "valor";
+let y = "valor";
+
+let a = 20;
+let b = 5.4;
+let string = "Mi nombre es Mateo estiven"
+let boolean_one = false;
+let boolean_two = true;
+let array = [1,2,3,5,4];
+let array_string = ["lunes","martes","miercoles","jueves","viernes"];
+
+//operadores aritmeticos: +-/*%
+let suma = 1+5+a;
+let resta = 1-5-a;
+let multi = 1*5*a;
+let divi = 1/5/a;
+let module = 10%2;
+let elev = 5^2;
+console.log("- "+suma+"\n- "+resta+"\n- "+multi+"\n- "+div+"\n- "+module+"\n- "+elev);
+
+//operadores de comparacion = / == / === / <> / >= / <= / < / >
+//operadores logicos and && / or || /
+if (10>5){
+    for (let i=0; i<array_string.length; i++){
+        console.log(array_string[]);
+    }
+}
+
+let j = 5;
+let k = 10;
+while(k>=j){
+    console.log("Entra");
+    j++;
+}
